34c4a91481f8f00e73c07fe92e1cfcd833e148f6
Zachary Soja <zachary3003@gmail.com>
2023-03-18 13:59:22 +0100

    feature: add endpoint and integration tests

---
+++ Messenger.Api/Controllers/DirectMessageController.cs
@@ -0,0 +1,64 @@
+﻿using MediatR;
+using Messenger.Api.Infrastructure;
+using Messenger.Application.Command;
+using Messenger.Models.DirectMessage;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Messenger.Api.Controllers
+{
+    [Authorize]
+    [Route("/api/direct-message")]
+    public class DirectMessageController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public DirectMessageController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        /// <summary>
+        /// Adds new direct message with data given in request
+        /// </summary>
+        /// <response code="204">Message added successfully</response>
+        /// <response code="400">Failed to add the message</response>
+        [HttpPost]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        public async Task<ActionResult> PostAsync(AddDirectMessageCommand command)
+        {
+            var res = await _mediator.Send(command);
+
+            return res.ReturnNoContentOrBadRequest();
+        }
+
+        /// <summary>
+        /// Returns list of messages
+        /// </summary>
+        /// <response code="200">List of messages</response>
+        [HttpGet]
+        [ProducesResponseType(200)]
+        public async Task<ActionResult<IEnumerable<DirectMessageModel>>> GetAsync([FromQuery] GetDirectMessagesQuery query)
+        {
+            var res = await _mediator.Send(query);
+
+            return Ok(res);
+        }
+
+        /// <summary>
+        /// Updates message with given data
+        /// </summary>
+        /// <response code="204">Message updated successfully</response>
+        /// <response code="400">Failed to update the message</response>
+        [HttpPut]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        public async Task<ActionResult> PutAsync(UpdateDirectMessageCommand command)
+        {
+            var res = await _mediator.Send(command);
+
+            return res.ReturnNoContentOrBadRequest();
+        }
+    }
+}
---
+++ Messenger.Tests.Integration/Controller/DirectMessageControllerTest.cs
@@ -0,0 +1,98 @@
+﻿using Messenger.Application.Command;
+using Messenger.Domain.Entity;
+using Messenger.Models.DirectMessage;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace Messenger.Tests.Integration.Controller
+{
+    public class DirectMessageControllerTest : ControllerTest
+    {
+        const string ApiUrl = "/api/direct-message";
+
+        private void InsertMessages(IEnumerable<DirectMessage> messages)
+        {
+            foreach (var message in messages)
+            {
+                ExecuteQuery("INSERT INTO [DirectMessage]([Content], [Created], [SenderId], [ReceiverId], [Read])" +
+                    " VALUES(@Content, @Created, @SenderId, @ReceiverId, @Read)", message);
+            }
+        }
+
+        [Fact]
+        public async Task GetAsync_Success()
+        {
+            await Authorize();
+
+            foreach (var user in FakeDataFactory.CreateUsers(2))
+            {
+                InsertUser(user);
+            }
+
+            var users = GetFromDatabase<User>("SELECT * FROM [User] WHERE [Id]!=@Id", new { Id = _authorizedUserId });
+
+            var sender = users.First();
+            var receiver = users.Last();
+
+            InsertMessages(FakeDataFactory.CreateMessages(sender.Id, receiver.Id, 4));
+            InsertMessages(FakeDataFactory.CreateMessages(receiver.Id, sender.Id, 4));
+            InsertMessages(FakeDataFactory.CreateMessages(21, 37, 5));
+
+            var messages = GetFromDatabase<DirectMessage>("SELECT * FROM [DirectMessage]")
+                .Where(x => (x.SenderId == sender.Id && x.ReceiverId == receiver.Id)
+                || (x.SenderId == receiver.Id && x.ReceiverId == sender.Id)).ToList();
+
+            var response = await _httpClient.GetAsync($"{ApiUrl}?User1Id={sender.Id}&User2Id={receiver.Id}");
+            var content = await response.Content.ReadFromJsonAsync<IEnumerable<DirectMessageModel>>();
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal(8, content.Count());
+            Assert.Equivalent(users.Select(x => x.Name), content.Select(x => x.SenderName));
+            Assert.Equivalent(messages.Select(x => x.Id), content.Select(x => x.Id));
+        }
+
+        [Fact]
+        public async Task PostAsync_Success()
+        {
+            await Authorize();
+
+            var request = new AddDirectMessageCommand
+            {
+                Content = "Content",
+                ReceiverId = 1,
+                SenderId = _authorizedUserId
+            };
+
+            var response = await _httpClient.PostAsJsonAsync(ApiUrl, request);
+
+            var messages = GetFromDatabase<DirectMessage>("SELECT * FROM [DirectMessage]");
+
+            Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+            Assert.Contains(messages, x => x.Content == request.Content && x.ReceiverId == request.ReceiverId && x.SenderId == request.SenderId);
+            Assert.Single(messages);
+        }
+
+        [Fact]
+        public async Task PutAsync_Success()
+        {
+            await Authorize();
+
+            InsertMessages(FakeDataFactory.CreateMessages(_authorizedUserId, 2, 2));
+
+            var idToUpdate = GetFromDatabase<long>("SELECT [Id] FROM [DirectMessage]").First();
+
+            var request = new UpdateDirectMessageCommand
+            {
+                Id = idToUpdate,
+                Read = true
+            };
+
+            var response = await _httpClient.PutAsJsonAsync(ApiUrl, request);
+
+            var messages = GetFromDatabase<DirectMessage>("SELECT * FROM [DirectMessage]");
+
+            Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+            Assert.Contains(messages, x => x.Id == idToUpdate && x.Read);
+        }
+    }
+}
