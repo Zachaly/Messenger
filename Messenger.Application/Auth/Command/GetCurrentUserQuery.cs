dc8e6766f91d4842a217458b786e809a1d0bf488
Zachary Soja <zachary3003@gmail.com>
2023-03-20 18:26:51 +0100

    feature: add command

---
+++ Messenger.Application/Auth/Command/GetCurrentUserQuery.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Messenger.Models.User;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messenger.Application.Auth.Command
+{
+    public class GetCurrentUserQuery : IRequest<LoginResponse>
+    {
+
+    }
+
+    public class GetCurrentUserHandler : IRequestHandler<GetCurrentUserQuery, LoginResponse>
+    {
+        public GetCurrentUserHandler(IHttpContextAccessor)
+        {
+
+        }
+
+        public Task<LoginResponse> Handle(GetCurrentUserQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
