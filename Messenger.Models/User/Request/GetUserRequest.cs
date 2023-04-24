e39ab95623d06a26aa58888bb15344fe98b168a1
Zachary Soja <zachary3003@gmail.com>
2023-04-24 23:58:23 +0200

    feature: add backend

--- Messenger.Models/User/Request/GetUserRequest.cs
+++ Messenger.Models/User/Request/GetUserRequest.cs
@@ -1,7 +1,11 @@
-﻿namespace Messenger.Models.User.Request
+﻿using Messenger.Domain.SqlAttributes;
+
+namespace Messenger.Models.User.Request
 {
     public class GetUserRequest : PagedRequest
     {
         public long? Id { get; set; }
+        [Where(Column = "[User].[Name] LIKE ", ContentWrapper = "%")]
+        public string? SearchName { get; set; }
     }
 }
